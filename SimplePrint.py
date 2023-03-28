4e0a3481365a651e6451fa37c19747b472a5c698
maryam.bromande <www.mrymjryty@gmail.com>
2023-03-28 15:01:57 +0430

    mbs

--- SimplePrint.py
+++ SimplePrint.py
@@ -1,3 +1,17 @@
-# please write a simple print in python
-print("maryam")
-print("test")
+# # please write a simple print in python
+for i in range(10+1):
+    print( '*'* i)
+
+print("""mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm""")
+
+for i in range(6):
+    print("*"*15,)
+
+print("""mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm""")
+
+
+
+for i in range(7):
+    print("*"*50)
+
+print("""mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm""")
