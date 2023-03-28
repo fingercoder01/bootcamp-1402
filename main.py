337d3d67fa70f29299e8356f14bbd60a546bde41
maryam.bromande <www.mrymjryty@gmail.com>
2023-03-28 11:37:46 +0430

    mbs

--- main.py
+++ main.py
@@ -10,4 +10,15 @@ print('hello khanom jorati')
 
 print('git')
 print("maryam bromande")
-Aa_12 = "maryam"
+Aa_1 = 'hello'
+Bb_1 = "hello"
+Cc_1 = """hello
+    hello
+          hello"""
+_maryam = 12 + 13 + 14 + 38 +18\
+    + 12 + 16 + 45 + 99 +\
+    11 + 12 + 88 +99
+print(Aa_1)
+print(Bb_1)
+print(Cc_1)
+print(_maryam)
