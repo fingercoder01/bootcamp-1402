189262d6905a984b14c57437c088831df784a34a
maryam.bromande <www.mrymjryty@gmail.com>
2023-03-29 18:40:02 +0430

    Practice1

--- Operator.py
+++ Operator.py
@@ -13,21 +13,19 @@
 """
 num1 = 10
 num2 = 15
-print(num1,"+" ,num2,"=",num1+num2)
-print((num1,"-",num2,"=",num1-num2)
-print(num1,"/", num2,"=",num1/num2)
-print(num1 ,"//", num2,"=",num1//num2)
-print(num1, "*", num2,"=",num1*num2)
-print(num1, "==", num2,"=",num1==num2)
-print(num1  ,"!=", num2,"=",num1!=num2)
-print(num1 , ">", num2,"=",num1>num2)
-print(num1 , "<", num2,"=",num1<num2)
-print(num1 , ">=", num2,"=",num1>=num2)
-print(num1  ,"+<", num2,"="num1+<num2)
-num1 += num2
+print(num1,"+",num2,"=",num1+num2)
+print(num1,"-",num2,"=",num1-num2)
+print(num1,"*",num2,"=",num1*num2)
+print(num1,"/",num2,"=",num1/num2)
+print(num1,"//",num2,"=",num1//num2)
+print(num1,"%",num2,"=",num1%num2)
+print(num1,"==",num2,"=",num1==num2)
+print(num1,"!=",num2,"=",num1!=num2)
+print(num1,">",num2,"=",num1>num2)
+print(num1,"<",num2,"=",num1<num2)
+print(num1,">=",num2,"=",num1>=num2)
+print(num1,"<=",num2,"=",num1<=num2)
+num1 += 50
 print(num1)
-num2 -= num1
-print(num1)
-
-
-
+num2 -= 5
+print(num2)
